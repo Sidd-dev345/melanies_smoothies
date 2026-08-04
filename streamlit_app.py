3e26e22843749124f6586423ac38c26de188b9dc
Sidd-dev345 <siddharth.nair@nihilent.com>
2026-08-04 14:27:36 +0530

    Refactor Snowflake session handling in app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -13,9 +14,8 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie: ")
 st.write("The name on the Smoothie will be: ", name_on_order)
 
-cnx = st.connection('snowflake')
-session = cnx.session()
 
+session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
@@ -36,7 +36,7 @@ if ingredients_list:
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                     values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
-    #st.write(my_insert_stmt)
+    # st.write(my_insert_stmt)
 
     time_to_insert = st.button('Submit Order')
 
@@ -46,9 +46,3 @@ if ingredients_list:
             f"Your Smoothie is ordered, {name_on_order}!",
             icon="✅"
         )
-
-
-
-
-
-
